f5ffb598b39ee9938a650b831f191eeca03675aa
Rogerio Sousa Moreira <doug7t@hotmail.com>
2025-03-06 22:47:50 -0300

    Teste tecnico

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+XDocument xml = XDocument.Load(@"C:\Users\doug\Documents\teste tecnico\dados.xml");
+
+
+List<double> faturamentos = xml.Descendants("row")
+    .Select(x => double.Parse(x.Element("valor")?.Value ?? "0"))
+    .Where(v => v > 0)
+    .ToList();
+
+if (faturamentos.Count == 0)
+{
+    Console.WriteLine("Nenhum dia com faturamento registrado.");
+    return;
+}
+
+// Calcula menor e maior faturamento
+double menorFaturamento = faturamentos.Min();
+double maiorFaturamento = faturamentos.Max();
+
+// Calcula a média dos dias com faturamento
+double media = faturamentos.Average();
+
+// Conta quantos dias tiveram faturamento acima da média
+int diasAcimaDaMedia = faturamentos.Count(v => v > media);
+
+// Exibe os resultados
+Console.WriteLine($"Menor faturamento: {menorFaturamento:F2}");
+Console.WriteLine($"Maior faturamento: {maiorFaturamento:F2}");
+Console.WriteLine($"Dias com faturamento acima da média: {diasAcimaDaMedia}");
